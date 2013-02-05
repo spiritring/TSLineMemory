0428f45565d8f4dbfedcfa4e0b07ad096428da2a
Enel <spiritring@gmail.com>
2013-02-05 21:22:08 +0800

    Initial Commit

---
+++ TSLineMemory/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TSLineMemory
+//
+//  Created by TSEnel on 13-2-5.
+//  Copyright (c) 2013年 TSEnel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
